c089bca32f7a6011c4857bcd2fde28592ef734b9
Levi Zitting <levizitting5663@gmail.com>
2018-10-24 21:25:26 -0500

    start and answer call

--- public/js/main.js
+++ public/js/main.js
@@ -11,14 +11,41 @@ document.addEventListener('DOMContentLoaded', () => {
         window.attachStreamToVideo(stream, localVideo);
     });
 
+    let callAnswered = false;
+
     startCallButton.addEventListener('click', startCall);
 
     function startCall() {
         startCallButton.style.display = 'none';
 
+        const peer1 = new window.SimplePeer({ initiator: true, stream: webcamStream });
+
+        peer1.on('signal', sdp => {
+            if (callAnswered) return;
+        });
+
+        function callAnswered(remoteSdp) {
+            peer1.signal(remoteSdp);
+            callAnswered = true;
+        }
+
+        peer1.on('stream', stream => {
+            window.attachStreamToVideo(stream, remoteVideo);
+        });
     }
 
     function answerCall() {
+        startCallButton.style.display = 'none';
 
+        const peer2 = new window.SimplePeer({ initiator: false,  stream: webcamStream });
+
+        peer2.signal(remoteSdp);
+        peer2.on('signal', sdp => {
+
+        });
+
+        peer2.on('stream', stream => {
+            window.attachStreamToVideo(stream, remoteVideo)
+        })
     }
 });
