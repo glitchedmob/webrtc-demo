5ee6ced051216a79b422704a2eec5f2e38abc61d
Levi Zitting <levizitting5663@gmail.com>
2018-10-24 20:52:43 -0500

    start

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const socket = window.io();
 
     const remoteVideo = document.querySelector('#remote');
     const localVideo = document.querySelector('#local');
@@ -7,51 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let webcamStream = null;
 
-    window.getWebcam({ video: true, audio: false }).then(stream => {
-        webcamStream = stream;
-        window.attachStreamToVideo(stream, localVideo);
-    });
-
-    let callAnswered = false;
-
     startCallButton.addEventListener('click', startCall);
 
     function startCall() {
-        startCallButton.style.display = 'none';
-        const peer1 = new window.SimplePeer({ initiator: true, stream: webcamStream, trickle: false, });
 
-        peer1.on('signal', sdp => {
-            if (callAnswered) return;
-            socket.emit('startCall', JSON.stringify(sdp));
-        });
-
-        socket.on('callAnswered', remoteSdp => {
-            peer1.signal(remoteSdp);
-            callAnswered = true;
-        });
-
-        peer1.on('stream', stream => {
-            window.attachStreamToVideo(stream, remoteVideo);
-        });
     }
 
-    function answerCall(remoteSdp) {
-        startCallButton.style.display = 'none';
+    function answerCall() {
 
-        const peer2 = new window.SimplePeer({ initiator: false,  stream: webcamStream, trickle: false });
-
-        peer2.signal(remoteSdp);
-        peer2.on('signal', sdp => {
-            if (callAnswered) return;
-            socket.emit('answerCall', JSON.stringify(sdp));
-        });
-
-
-        peer2.on('stream', stream => {
-            window.attachStreamToVideo(stream, remoteVideo)
-        })
     }
 
 
-    socket.on('callStarted', answerCall);
 });
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const http = require('http');
-const socketIO = require('socket.io');
 
 const PORT = 8000;
 
 const app = express();
 const server = http.Server(app);
-const io = socketIO(server);
 
 app.use(express.static('public'));
 
@@ -17,16 +15,3 @@ app.get('/', function(req, res){
 server.listen(PORT, function(){
     console.log(`listening on http://localhost:${PORT}`);
 });
-
-io.on('connection', socket => {
-    socket.on('startCall', data => {
-        console.log('startCall');
-        socket.broadcast.emit('callStarted', data)
-    });
-
-    socket.on('answerCall', data => {
-        console.log('answerCall');
-        socket.broadcast.emit('callAnswered', data)
-    });
-});
-
